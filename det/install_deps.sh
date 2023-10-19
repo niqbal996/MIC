e3329646d49b19c670eea55e3cb325cc50a163d4
Naeem Iqbal <naeemiqbal996@gmail.com>
2023-10-19 16:25:22 +0200

    Fixed dependencies with PyTorch1.12

---
+++ det/install_deps.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+apt update 
+apt install -y nano python3.8 python3-pip python-is-python3
+python -m pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113
+apt-get install ffmpeg libsm6 libxext6  -y
+python3 -m pip install -r requirements.txt
