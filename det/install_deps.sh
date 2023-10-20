6a1c76e27d5ada4b074196705a8285043fc0274d
Naeem Iqbal <naeemiqbal996@gmail.com>
2023-10-20 11:52:22 +0200

    Compiling maskRcnn again before every training. Optional!

--- det/install_deps.sh
+++ det/install_deps.sh
@@ -3,4 +3,5 @@ apt update
 apt install -y nano python3.8 python3-pip python-is-python3
 python -m pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113
 apt-get install ffmpeg libsm6 libxext6  -y
-python3 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
+python setup.py develop
